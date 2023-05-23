676683f80f0b455d1530edadd2091c5c7df68eb0
Rafael <raaf.marques019@gmail.com>
2023-05-23 12:04:40 -0300

    FarmaGold - Farmácia ATUALIZADO

--- src/main/java/com/generation/farmagold/controller/ProdutoController.java
+++ src/main/java/com/generation/farmagold/controller/ProdutoController.java
@@ -20,32 +20,27 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    // Metodo para lista todos os produtos ja cadastrados
     @GetMapping
     public ResponseEntity<List<Produto>> getAll() {
         return ResponseEntity.ok(produtoRepository.findAll());
     }
 
-    // Metodo para listar um produto buscando pelo id ja definido
     @GetMapping("/{id}")
     public ResponseEntity<Produto> getById(@PathVariable Long id) {
         return produtoRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    // Metodo para buscar um produto pelo nome
     @GetMapping("/buscar/{nome}")
     public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
         return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
     }
 
-    // Metodo para adicionar um novo produto
     @PostMapping("/adicionar")
     public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
     }
 
-    // Metodo para editar um produto ja existente
     @PutMapping("/editar")
     public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
         return produtoRepository.findById(produto.getId())
@@ -53,7 +48,6 @@ public class ProdutoController {
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    // Metodo para deletar um produto ja existente
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/deletar/{id}")
     public void delete(@PathVariable Long id) {
