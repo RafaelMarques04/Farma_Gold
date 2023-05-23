59bfcbcbc348f6e98eff0fd9195540b21e1c7c81
Rafael <raaf.marques019@gmail.com>
2023-05-23 11:59:47 -0300

    Farma Gold - Farmácia

---
+++ src/main/java/com/generation/farmagold/FarmagoldApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmagold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmagoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmagoldApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/farmagold/model/Produto.java
@@ -0,0 +1,44 @@
+package com.generation.farmagold.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String nome;
+
+	@NotNull
+	private Double preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
